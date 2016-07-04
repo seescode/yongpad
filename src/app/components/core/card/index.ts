ee504ce0691838791e986e85e598225b76fc033d
seescode <seesyong@gmail.com>
2016-07-03 22:02:58 -0400

    removed card component

--- src/app/components/core/card/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the lazy loaded Card.
- */
-export * from './card.component';
--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,12 +1,10 @@
 import {Component} from "@angular/core";
-import {CardComponent} from "../core/card/card.component";
 
 @Component({
     selector: 'edit',
     moduleId: module.id,
     templateUrl: './edit.component.html',
-    styleUrls: ['./edit.component.css'],
-    directives: [CardComponent]
+    styleUrls: ['./edit.component.css']
 })
 export class EditComponent {
 }
