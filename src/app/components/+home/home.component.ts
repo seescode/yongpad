5d87c9af10f3fb645a91c8d8ee2d4f5ceb9f4807
seescode <seesyong@gmail.com>
2016-07-13 23:20:43 -0400

    Support deletions

--- src/app/components/+home/home.component.ts
+++ src/app/components/+home/home.component.ts
@@ -3,18 +3,20 @@ import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {TasksService} from '../../shared/services/src/tasks.service'
 
 @Component({
-    selector: 'home',
-    moduleId: module.id,
-    directives: [ROUTER_DIRECTIVES],    
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  moduleId: module.id,
+  directives: [ROUTER_DIRECTIVES],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   notes: string[]
 
   constructor(tasksSvc: TasksService) {
-    this.notes = tasksSvc.get();    
+    this.notes = tasksSvc.get();
   }
 
-
+  delete(index: number) {
+    this.notes.splice(index, 1);
+  }
 }
